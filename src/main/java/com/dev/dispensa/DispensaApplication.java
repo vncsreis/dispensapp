0a3f6546ffd5650fee32a515855baa5de58ed971
dev <dev@MESA-8>
2023-04-18 17:44:36 -0300

    initial commit

---
+++ src/main/java/com/dev/dispensa/DispensaApplication.java
@@ -0,0 +1,12 @@
+package com.dev.dispensa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispensaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispensaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dev/dispensa/DispensaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.dispensa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispensaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
