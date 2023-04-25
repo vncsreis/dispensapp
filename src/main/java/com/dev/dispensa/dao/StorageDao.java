e91107a323c4ac56ca3ae028262ed5bba26d359d
vinicius <viniciusr66.vr@gmail.com>
2023-04-25 16:43:04 -0300

    create user and storage dao

---
+++ src/main/java/com/dev/dispensa/dao/StorageDao.java
@@ -0,0 +1,14 @@
+package com.dev.dispensa.dao;
+
+public class StorageDao {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
---
+++ src/main/java/com/dev/dispensa/dao/UserDao.java
@@ -0,0 +1,24 @@
+package com.dev.dispensa.dao;
+
+public class UserDao {
+
+  private String name;
+  private String email;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+}
